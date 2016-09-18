1f53825065374f52af2967d7b62c1ed7e70b9fb5
paullang <paul.andrew.lang@gmail.com>
2016-09-17 20:53:30 -0700

    Output js & map files to js folder

--- systemjs.config.js
+++ systemjs.config.js
@@ -10,8 +10,8 @@
     },
     // map tells the System loader where to look for things
     map: {
-      // our app is within the app folder
-      app: 'app',
+      // our app is within the js folder
+      app: 'js',
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
